9e724eeea39d073da781d2950109ff70ce038472
Alexiy Buynitsky <abuynits@gmail.com>
2023-04-05 13:17:15 -0400

    added comments for deep-esn code

--- src/ESN.py
+++ src/ESN.py
@@ -30,7 +30,6 @@ class ESN(nn.Module):
     all: entire reservoir output matrix is used
     mean: mean of reservoir output matrix along with timesteps is used
     last: only the last timestep of reservoir output matrix is used
-
     """
 
     def __init__(self,
@@ -61,7 +60,7 @@ class ESN(nn.Module):
         elif non_linearity == 'relu':
             self._mode = 'RES_RELU'
         elif non_linearity == 'id':
-            self._mode = 'RES)ID'
+            self._mode = 'RES_ID'
         else:
             raise ValueError("Linearity '{}' not found.".format(non_linearity))
 
@@ -114,3 +113,135 @@ class ESN(nn.Module):
         self.XTX = None
         self.XTy = None
         self.X = None
+
+    def forward(self, input, washout, h_0=None, target=None):
+        with torch.no_grad():
+            is_packed = isinstance(input, PackedSequence)
+
+            output, hidden = self.reservoir(input, h_0)
+            if is_packed:
+                # if have sequences of unequal length, pad the output to fit it
+                output, seq_lengths = pad_packed_sequence(output,
+                                                          batch_first=self.batch_first)
+            else:
+                if self.batch_first:
+                    # perform in paralel: have the batch (number of samples) times the batch eln
+                    seq_lengths = output.size(0) * [output.size(1)]
+                else:
+                    # batch has been transposed: have sequences of size(1) and batch is added on
+                    seq_lengths = output.size(1) * [output.size(0)]
+
+            # want to have the first dim be the number of examples in a sequence
+            if self.batch_first:
+                output = output.transpose(0, 1)
+
+            output, seq_lengths = washout_tensor(output, washout, seq_lengths)
+
+            if self.w_io:
+                if is_packed:
+                    input, input_lengths = pad_packed_sequence(input,
+                                                               batch_first=self.batch_first)
+                else:
+                    input_lengths = [input.size(0)] * input.size(1)
+
+                if self.batch_first:
+                    input = input.transpose(0, 1)
+
+                input, _ = washout_tensor(input, washout, input_lengths)
+                output = torch.cat([input, output], -1)
+
+            if self.readout_training == 'gd' or target is None:
+                with torch.enable_grad():
+                    output = self.readout(output)
+
+                    if is_packed:
+                        for i in range(output.size(1)):
+                            if seq_lengths[i] < output.size(0):
+                                output[seq_lengths[i]:, i] = 0
+
+                    if self.batch_first:
+                        output = output.transpose(0, 1)
+
+                    # Uncomment if you want packed output.
+                    # if is_packed:
+                    #     output = pack_padded_sequence(output, seq_lengths,
+                    #                                   batch_first=self.batch_first)
+
+                    return output, hidden
+
+            else:
+                batch_size = output.size(1)
+
+                X = torch.ones(target.size(0), 1 + output.size(2), device=target.device)
+                row = 0
+                for s in range(batch_size):
+                    if self.output_steps == 'all':
+                        X[row:row + seq_lengths[s], 1:] = output[:seq_lengths[s],
+                                                          s]
+                        row += seq_lengths[s]
+                    elif self.output_steps == 'mean':
+                        X[row, 1:] = torch.mean(output[:seq_lengths[s], s], 0)
+                        row += 1
+                    elif self.output_steps == 'last':
+                        X[row, 1:] = output[seq_lengths[s] - 1, s]
+                        row += 1
+
+                if self.readout_training == 'cholesky':
+                    if self.XTX is None:
+                        self.XTX = torch.mm(X.t(), X)
+                        self.XTy = torch.mm(X.t(), target)
+                    else:
+                        self.XTX += torch.mm(X.t(), X)
+                        self.XTy += torch.mm(X.t(), target)
+
+                elif self.readout_training == 'svd':
+                    # Scikit-Learn SVD solver for ridge regression.
+                    U, s, V = torch.svd(X)
+                    idx = s > 1e-15  # same default value as scipy.linalg.pinv
+                    s_nnz = s[idx][:, None]
+                    UTy = torch.mm(U.t(), target)
+                    d = torch.zeros(s.size(0), 1, device=X.device)
+                    d[idx] = s_nnz / (s_nnz ** 2 + self.lambda_reg)
+                    d_UT_y = d * UTy
+                    W = torch.mm(V, d_UT_y).t()
+
+                    self.readout.bias = nn.Parameter(W[:, 0])
+                    self.readout.weight = nn.Parameter(W[:, 1:])
+                elif self.readout_training == 'inv':
+                    self.X = X
+                    if self.XTX is None:
+                        self.XTX = torch.mm(X.t(), X)
+                        self.XTy = torch.mm(X.t(), target)
+                    else:
+                        self.XTX += torch.mm(X.t(), X)
+                        self.XTy += torch.mm(X.t(), target)
+
+                return None, None
+
+
+def washout_tensor(tensor, washout, seq_lengths, bidirectional=False, batch_first=False):
+    # create a tensor such that the batch is the second dim
+    tensor = tensor.transpose(0, 1) if batch_first else tensor.clone()
+    # handle different types of inputs for the sequence
+    if type(seq_lengths) == list:
+        seq_lengths = seq_lengths.copy()
+    if type(seq_lengths) == torch.Tensor:
+        seq_lengths = seq_lengths.clone()
+    # batch is now the second dimension
+    for b in range(tensor.size(1)):
+        if washout[b] > 0:
+            tmp = tensor[washout[b]:seq_lengths[b], b].clone()
+            tensor[:seq_lengths[b] - washout[b], b] = tmp
+            tensor[seq_lengths[b] - washout[b]:, b] = 0
+            seq_lengths[b] -= washout[b]
+
+            if bidirectional:
+                tensor[seq_lengths[b] - washout[b]:, b] = 0
+                seq_lengths[b] -= washout[b]
+    # account for output of the sequence
+    if type(seq_lengths) == list:
+        max_len = max(seq_lengths)
+    else:
+        max_len = max(seq_lengths).item()
+
+    return tensor[:max_len], seq_lengths
